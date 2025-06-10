3eb6a4862f3616cd235da380beed7c0293b61fdb
mansisonar279 <mansirsonar78@gmail.com>
2025-06-10 08:02:26 +0530

    save

---
+++ Score.java
@@ -0,0 +1,22 @@
+public class Score {
+   
+        public static void main(String[] args) {
+            int[] scores = {85, 90, 78, 92, 88}; 
+            int sum = 0;
+            int max = scores[0];
+            int min = scores[0];
+    
+            for (int score : scores) {
+                sum += score;
+            }
+            
+    
+            double average = (double) sum / scores.length;
+    
+            System.out.println("Sum: " + sum);
+            System.out.println("Average: " + average);
+        }
+    }
+    
+    
+
