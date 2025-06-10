51148d8ec6aa10ff75ec334479636988cf4978c4
mansisonar0279 <mansirsonar78@gmail.com>
2025-06-10 07:53:50 +0530

    save

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int[] scores = {85, 90, 78, 92, 88}; 
+        int sum = 0;
+        
+
+        for (int score : scores) {
+            sum += score;
+        }
+        
+
+        double average = (double) sum / scores.length;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + average);
+    }
+}
